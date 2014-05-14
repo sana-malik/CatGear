06abf6e171f4d4436a7bf58bf276857ce5cc1d60
sana-malik <heyletsdance@gmail.com>
2014-05-14 18:16:33 -0400

    add timestamp and speed controls

--- Visualization/scripts/cattracks.js
+++ Visualization/scripts/cattracks.js
@@ -8,7 +8,7 @@ d3.cattracks = function() {
       svg = d3.select("#main").append("svg").attr("width", width).attr("height", height+timeHeight+30),
       index = 0,
       timescale = d3.time.scale(),
-      duration = 1000;
+      duration = 500;
 
   cattracks.points = function(_) {
     if (!arguments.length) return points;
@@ -93,6 +93,8 @@ d3.cattracks = function() {
       .attr("height", timeHeight)
       .attr("transform", "translate(" + timescale(points[index]["time"]) + "," + height + ")");
 
+    $("#timestamp").text(points[index]["time"].format("MMMM Do YYYY, h:mm a"));
+
     // place cat markers
     var oranges = svg.append("circle")
       .attr("id", "oranges")
@@ -116,7 +118,8 @@ d3.cattracks = function() {
       timeMarker.transition()
         .duration(duration)
         .attr("transform",  "translate(" + timescale(points[index]["time"]) + "," + height + ")");
-
+      $("#timestamp").text(points[index]["time"].format("MMMM Do YYYY, h:mm a"));
+      
       // update cat markers
       var pathId = "#" + points[index-1]["oranges"] + "-" + points[index]["oranges"];
       oranges.transition()
@@ -139,6 +142,16 @@ d3.cattracks = function() {
         }
       }
     }
+
+    return cattracks;
+  };
+
+  cattracks.speed_up = function() {
+    duration /= 1.5;
+  };
+
+  cattracks.slow_down = function() {
+    duration *= 1.5;
   };
 
   return cattracks;
--- Visualization/scripts/controller.js
+++ Visualization/scripts/controller.js
@@ -5,7 +5,7 @@ function parseData(data) {
 	var dataObjs = [];
 	$.each(data, function(index, obj) {
 		var tmp = {};
-		tmp["time"] = moment(obj["time"], "YYYY-MM-DDTHH:mm:ssZ").toDate();
+		tmp["time"] = moment(obj["time"], "YYYY-MM-DDTHH:mm:ssZ");
 		tmp["oranges"] = obj["oranges"];
 		tmp["greyest"] = obj["greyest"];
 		dataObjs.push(tmp);
@@ -22,4 +22,12 @@ $(document).ready(function() {
 			.points(data)
 			.paths(locations)
 			.runAnimation();
+
+	$("#speed_up").on("click", function() {
+		cattracks.speed_up();
+	})
+
+	$("#slow_down").on("click", function() {
+		cattracks.slow_down();
+	})
 });
